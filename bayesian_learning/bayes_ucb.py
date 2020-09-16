171d03637ba8adfe6b9c806fef43a6a92820d072
johan-kallstrom <mr.johan.kallstrom@gmail.com>
2020-09-16 21:09:28 +0200

    Added sliding window versions of Bayesian algorithms.

--- bayesian_learning/bayes_ucb.py
+++ bayesian_learning/bayes_ucb.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+from collections import deque 
 from scipy.stats import beta
 
 class BayesUcb:
@@ -35,3 +36,36 @@ class BayesUcb:
 
         # beta = beta + abs(reward - 1)
         self.posteriors[arm][1] += abs(reward - 1)
+
+class SlidingWindowBayesUcb:
+
+    def __init__(self, priors, window_length=100):
+        self.priors = priors
+        self.n_arms = len(priors)
+        self.warmup_arm = 0
+        self.time_step = 0
+        self.recent_successes = [deque(maxlen=window_length) for _ in range(self.n_arms)]
+        self.recent_failures = [deque(maxlen=window_length) for _ in range(self.n_arms)]
+
+    def select_arm(self):
+        if self.warmup_arm < self.n_arms:
+            arm = self.warmup_arm
+            self.warmup_arm += 1
+        else:
+            qs = np.zeros(shape=(self.n_arms,))
+            for i in range(self.n_arms):
+                p = 1 - 1 / self.time_step
+                q = beta.ppf(p, a=self.priors[i][0]+np.sum(self.recent_successes[i]), b=self.priors[i][1]+np.sum(self.recent_failures[i]), loc=0, scale=1)
+                qs[i] = q
+            arm = np.argmax(qs)
+        self.time_step += 1
+        return arm
+
+    def learn(self, arm, reward):
+        # Update alpha and beta in Beta distribution
+
+        # alpha = alpha + reward
+        self.recent_successes.append(reward)
+
+        # beta = beta + abs(reward - 1)
+        self.recent_failures.append(abs(reward - 1))
--- bayesian_learning/thompson_sampling.py
+++ bayesian_learning/thompson_sampling.py
@@ -2,14 +2,13 @@
 
 import numpy as np
 
-from scipy.stats import beta
+from collections import deque
 
 class ThompsonSampling:
 
     def __init__(self, priors):
         self.posteriors = priors
         self.n_arms = len(priors)
-        # self.rng = np.random.default_rng()
 
     def select_arm(self):
         draws = np.zeros(shape=(self.n_arms,))
@@ -27,6 +26,34 @@ class ThompsonSampling:
 
         # beta = beta + abs(reward - 1)
         self.posteriors[arm][1] += abs(reward - 1)
+
+    def plot_posteriors(self, save_to_file=False):
+        pass
+
+class SlidingWindowThompsonSampling:
+
+    def __init__(self, priors, window_length=100):
+        self.priors = priors
+        self.n_arms = len(priors)
+        self.recent_successes = [deque(maxlen=window_length) for _ in range(self.n_arms)]
+        self.recent_failures = [deque(maxlen=window_length) for _ in range(self.n_arms)]
+
+    def select_arm(self):
+        draws = np.zeros(shape=(self.n_arms,))
+        for i in range(self.n_arms):
+            theta = np.random.beta(a=self.priors[i][0]+np.sum(self.recent_successes[i]), b=self.priors[i][1]+np.sum(self.recent_failures[i]))
+            draws[i] = theta
+        arm = np.argmax(draws)
+        return arm
+
+    def learn(self, arm, reward):
+        # Update alpha and beta in Beta distribution
+
+        # alpha = alpha + reward
+        self.recent_successes.append(reward)
+
+        # beta = beta + abs(reward - 1)
+        self.recent_failures.append(abs(reward - 1))
 
     def plot_posteriors(self, save_to_file=False):
         pass
